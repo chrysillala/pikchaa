7ad6de23f2569380075c2ba7bb41ad47c9c48db2
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2023-06-10 14:08:06 +0700

    update app js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+import { useState } from "react";
+import "@/styles/globals.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+import { GlobalProvider } from "@/contexts/GlobalContext";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <GlobalProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}></Hydrate>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </GlobalProvider>
+  );
 }
