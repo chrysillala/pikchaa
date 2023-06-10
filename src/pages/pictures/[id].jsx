043256694033ed91a420a454d4245c40b323f3de
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2023-06-10 14:14:14 +0700

    add ui for picture detail page

--- src/pages/pictures/[id].jsx
+++ src/pages/pictures/[id].jsx
@@ -1,7 +1,11 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
+
 import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
 
 import Layout from "@/components/Layout";
+import CardDetail from "@/components/Card/CardDetail";
 
 const PictureDetailPage = () => {
   const router = useRouter();
@@ -9,8 +13,22 @@ const PictureDetailPage = () => {
 
   return (
     <Layout>
-      <Toolbar />
-      <p>Picture Detail {id}</p>
+      <Head>
+        <title>Picture Detail</title>
+      </Head>
+      <Container
+        maxWidth="md"
+        sx={{
+          my: 4,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Toolbar />
+        <CardDetail id={id} />
+      </Container>
     </Layout>
   );
 };
